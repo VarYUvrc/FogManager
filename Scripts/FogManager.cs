f5f8e51decdc6b5d9dda2d3485634b72383d5e92
VarYU <varyu.vrc@gmail.com>
2023-08-12 21:57:41 +0900

    ✨ NightShiftモード追加

--- Scripts/FogManager.cs
+++ Scripts/FogManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-namespace Varyu.ExclusiveSpawner
+namespace Varyu.Fogmanager
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class FogManager : UdonSharpBehaviour
---
+++ Scripts/FogManagerNightShift.cs
@@ -0,0 +1,84 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Varyu.Fogmanager
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class FogManagerNightShift : UdonSharpBehaviour
+    {
+        [SerializeField] Color startColor;
+        [SerializeField] Color endColor;
+        [SerializeField] float startDensity = 1f;
+        [SerializeField] float endDensity = 2f;
+        [SerializeField] GameObject[] nightObject;
+        [SerializeField] GameObject[] dayObject;
+
+        private bool nightFlag = false;
+
+        void Start()
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = startColor;
+            RenderSettings.fogDensity = startDensity;
+        }
+
+        void Update()
+        {
+            if(nightFlag)
+            {
+                NightObjectShift();
+                NightFogShift();
+            }
+            else
+            {
+                DayObjectShift();
+                DayFogShift();
+            }
+        }
+
+        public override void Interact()
+        {
+            nightFlag = !nightFlag;
+        }
+
+        public void NightObjectShift()
+        {
+            foreach (GameObject nightobj in nightObject)
+            {
+                nightobj.SetActive(true);
+            }
+            foreach (GameObject dayobj in nightObject)
+            {
+                dayobj.SetActive(false);
+            }
+        }
+
+        public void NightFogShift()
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = endColor;
+            RenderSettings.fogDensity = endDensity;
+        }
+
+        public void DayObjectShift()
+        {
+            foreach (GameObject dayobj in nightObject)
+            {
+                dayobj.SetActive(true);
+            }
+            foreach (GameObject nightobj in nightObject)
+            {
+                nightobj.SetActive(false);
+            }
+        }
+
+        public void DayFogShift()
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = startColor;
+            RenderSettings.fogDensity = startDensity;
+        }
+    }
+}
